ab959df2869c6a29a04a288fcc29c66b6e783d85
Michal Kalis <michal.kalis@cleverlance.com>
2014-12-13 11:42:35 +0100

    Initial tabbed app template

---
+++ WeatherForecast/WeatherForecast/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherForecast
+//
+//  Created by Michal Kalis on 13/12/14.
+//  Copyright (c) 2014 strv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeatherForecast/WeatherForecast/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  WeatherForecast
+//
+//  Created by Michal Kalis on 13/12/14.
+//  Copyright (c) 2014 strv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
