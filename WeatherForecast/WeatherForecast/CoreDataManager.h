64f3af19f20d91f5ae503d5c18d32cb604550341
Michal Kalis <michal.kalis@cleverlance.com>
2014-12-13 15:32:13 +0100

    Core Data with manager

---
+++ WeatherForecast/WeatherForecast/CoreDataManager.h
@@ -0,0 +1,23 @@
+//
+//  CoreDataManager.h
+//  WeatherForecast
+//
+//  Created by Michal Kalis on 13/12/14.
+//  Copyright (c) 2014 strv. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@import CoreData;
+
+@interface CoreDataManager : NSObject
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *inMemoryManagedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
++ (CoreDataManager *)sharedManager;
+
+- (void)saveContext;
+
+@end
---
+++ WeatherForecast/WeatherForecast/CoreDataManager.m
@@ -0,0 +1,107 @@
+//
+//  CoreDataManager.m
+//  WeatherForecast
+//
+//  Created by Michal Kalis on 13/12/14.
+//  Copyright (c) 2014 strv. All rights reserved.
+//
+
+#import "CoreDataManager.h"
+
+@implementation CoreDataManager
+
+@synthesize managedObjectContext = _managedObjectContext;
+@synthesize inMemoryManagedObjectContext = _inMemmoryManagedObjectContext;
+@synthesize managedObjectModel = _managedObjectModel;
+@synthesize persistentStoreCoordinator = _persistentStoreCoordinator;
+
++ (CoreDataManager *)sharedManager {
+    static CoreDataManager *inst;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        inst = [[CoreDataManager alloc] init];
+    });
+    return inst;
+}
+
+- (void)saveContext {
+    NSError *error = nil;
+    NSManagedObjectContext *managedObjectContext = self.managedObjectContext;
+    if (managedObjectContext != nil) {
+        if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
+#warning TODO handle Core Data error
+            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+        }
+    }
+}
+
+#pragma mark - Core Data stack
+
+- (NSManagedObjectContext *)managedObjectContext {
+    if (_managedObjectContext != nil) {
+        return _managedObjectContext;
+    }
+    
+    NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
+    if (coordinator != nil) {
+        _managedObjectContext = [[NSManagedObjectContext alloc] init];
+        [_managedObjectContext setPersistentStoreCoordinator:coordinator];
+    }
+    return _managedObjectContext;
+}
+
+- (NSManagedObjectContext *)inMemoryManagedObjectContext {
+    if (_inMemmoryManagedObjectContext != nil) {
+        return _inMemmoryManagedObjectContext;
+    }
+    
+    NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
+    if (coordinator != nil) {
+        _inMemmoryManagedObjectContext = [[NSManagedObjectContext alloc] init];
+        [_inMemmoryManagedObjectContext setPersistentStoreCoordinator:coordinator];
+    }
+    return _inMemmoryManagedObjectContext;
+}
+
+- (NSManagedObjectModel *)managedObjectModel {
+    if (_managedObjectModel != nil) {
+        return _managedObjectModel;
+    }
+    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"CoreDataModel" withExtension:@"momd"];
+    _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
+    return _managedObjectModel;
+}
+
+- (NSPersistentStoreCoordinator *)persistentStoreCoordinator {
+    if (_persistentStoreCoordinator != nil) {
+        return _persistentStoreCoordinator;
+    }
+    
+    NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"WeatherForecast.sqlite"];
+    
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    // If the expected store doesn't exist, copy the default store.
+    if (![fileManager fileExistsAtPath:[storeURL path]]) {
+        NSURL *defaultStoreURL = [[NSBundle mainBundle] URLForResource:@"WeatherForecast" withExtension:@"sqlite"];
+        if (defaultStoreURL) {
+            [fileManager copyItemAtURL:defaultStoreURL toURL:storeURL error:NULL];
+        }
+    }
+    
+    _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
+    
+    NSError *error;
+    NSDictionary *options = @{NSMigratePersistentStoresAutomaticallyOption: @YES, NSInferMappingModelAutomaticallyOption: @YES};
+    if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:options error:&error]) {
+    }
+    
+    return _persistentStoreCoordinator;
+}
+
+#pragma mark - Application's Documents directory
+
+- (NSURL *)applicationDocumentsDirectory {
+    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+}
+
+@end
